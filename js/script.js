ed0aef917b687ab843eb5dd570db19b3a5f3a677
casey_h <caseyehurlburt@gmail.com>
2024-03-06 12:57:25 -0500

    Populated webpage with pet data by placing pet objects in an array and looping over objects using for of loop.

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ const createPet = function (name, species) {
             this.isTired = 1;
         },
         play: function () {
-            if (isTired === 10) {
+            if (this.isTired === 10) {
                 console.log("Too tired to play.");
                 return this.sleep();
             } else {
@@ -31,7 +31,31 @@ const francine = createPet("Francine", "turtle");
 
 // console.log(sora, clover, baxter, cleo, francine);
 
-clover.sleep();
-baxter.play();
+// clover.sleep();
+// baxter.play();
 
-console.log(clover, baxter);
+// console.log(clover, baxter);
+
+clover.isTired = 8;
+francine.isTired = 9;
+
+const allPets = [sora, clover, baxter, cleo, francine];
+// console.log(allPets);
+
+const showPets = function (petArray) {
+    pets.innerHTML = ""; // Clears list when showPets is run
+
+    for (let pet of petArray) {
+        let status = "ready to play!";
+        if (pet.isTired >= 7) {
+            status = "sleeping.";
+        }
+        const li = document.createElement("li");
+        li.innerHTML = `<span class="pet-name">${pet.name}</span> the ${pet.species} is ${status}.`;
+        pets.append(li);
+    }
+};
+
+statusButton.addEventListener("click", function () {
+    showPets(allPets);
+});
